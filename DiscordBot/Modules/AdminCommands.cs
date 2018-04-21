5533a442e4b29708a1172d35da2f3e947fd5833a
NeonPhoenix <neonphoenix.software@gmail.com>
2018-04-21 18:34:14 +1000

    Updated txt files and commands
    Added imgur links to all txt files, updated admin and info commands

--- DiscordBot/Modules/AdminCommands.cs
+++ DiscordBot/Modules/AdminCommands.cs
@@ -1,53 +1,29 @@
 ﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using DiscordBot.Init;
 
 namespace DiscordBot.Modules
 {
     public class AdminCommands : ModuleBase<SocketCommandContext>
     {
-        [Group("clean")]
-        [RequireUserPermission(GuildPermission.ManageChannels)]
-        public class CleanCommand : ModuleBase
+        [Command("kick")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task Kick(SocketGuildUser usr, [Remainder]string name)
         {
-            [Command]
-            public async Task CleanAsync()
-            {
-                
-            }
-
-            [Command]
-            public async Task CleanAsync(int count)
-            {
-                var msg = await Context.Channel.GetMessagesAsync(count + 1).Flatten();
-                await Context.Channel.DeleteMessagesAsync(msg);
-            }
-
-            [Command]
-            public async Task CleanAsync(IGuildUser user, int count = 100)
-            {
-
-            }
-        }
-
-        [Command("ban")]
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        public async Task BanAsync(IGuildUser user)
-        {
-
+            await ReplyAsync($"Goodbye {usr.Mention} :wave:");
+            await usr.KickAsync();
         }
 
         [Command("reloadimages")]
         [Summary("Reload Image list for SFW and NSFW Commands.")]
-        [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task ReloadImageAsync()
+        [RequireUserPermission(GuildPermission.ManageChannels)]
+        public async Task ReloadImage()
         {
             ImageInitialization.Clear();
             ImageInitialization.Init();
             await Context.Channel.SendMessageAsync("Reload Complete!");
         }
     }
-
-
 }

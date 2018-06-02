8a09a9ca4f18906fbce5ae3f8ee50e2226ed70fd
NeonPhoenix <neonphoenix.software@gmail.com>
2018-06-02 13:56:48 +1000

    Added More Reaction Commands
    implements #30

--- DiscordBot/Events/CommandMessageEvent.cs
+++ DiscordBot/Events/CommandMessageEvent.cs
@@ -6,37 +6,76 @@ namespace DiscordBot.Events
 {
     public class CommandMessageEvent
     {
-        public string RemoveStringFromString(string input, string charItem)
+        private string RemoveStringFromString(string input, string charItem)
         {
             var indexOfChar = input.IndexOf(charItem);
             if(indexOfChar < 0) { return input; }
             return RemoveStringFromString(input.Remove(indexOfChar, 1), charItem);
         }
 
+        private List<string> AddStringToString(List<string> input, char charItem)
+        {
+            List<string> output = new List<string>();
+            string[] tempArray;
+            string temp;
+
+            for (int i = 0; i < input.Count(); i++)
+            {
+                tempArray = input[i].Split(charItem);
+                temp = $"{tempArray[0]}@!{tempArray[1]}";
+                output.Add(temp);
+            }
+
+            return output;
+        }
+
+        public string GetAuthor(SocketCommandContext ctx) { return ctx.Message.Author.Username; }
+
         public string GetMentionedUsername(SocketCommandContext ctx)
         {
             IEnumerable<string> mentionedUser = ctx.Message.MentionedUsers.Select(x => x.Username);
-            string user;
-            if(mentionedUser.Count() > 1) { user = string.Join(" & ", mentionedUser); } else { user = string.Join(" ", mentionedUser); }
-            return user;
+            List<string> user = new List<string>();
+            string joined;
+
+            if(mentionedUser.Count() > 1)
+            {
+                for(int i = 0; i < mentionedUser.Count(); i++)
+                {
+                    user.Add(mentionedUser.ToString());
+                }
+            }
+            
+            if(mentionedUser.Count() > 1) { joined = string.Join(" & ", user); } else { joined = string.Join(" ", user); }
+
+            return joined;
         }
 
         public string CleanMessage(SocketCommandContext ctx, string[] msg)
         {
             IEnumerable<string> mentionedUserId = ctx.Message.MentionedUsers.Select(x => x.Mention);
 
-            string userId = string.Join(" ", mentionedUserId);
+            List<string> userId = new List<string>();
             string message = string.Join(" ", msg);
 
-            if(string.IsNullOrWhiteSpace(userId))
+            for(int i = 0; i < mentionedUserId.Count(); i++)
+            {
+                userId.Add(mentionedUserId.ToString());
+            }
+
+            if(userId.Count == 0)
             {
                 return message;
             }
             else
             {
-                userId = RemoveStringFromString(userId, "!");
-                message = RemoveStringFromString(message, "!");
-                message = message.Replace(userId, string.Empty);
+                userId = AddStringToString(userId, '@');
+
+                for(int a = 0; a < userId.Count(); a++)
+                {
+                    message = RemoveStringFromString(userId[a], string.Empty);
+                }
+
+                //message = message.Replace(userId, string.Empty);
                 return message;
             }
         }

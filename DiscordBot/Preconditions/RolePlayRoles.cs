ef2324dccc6b8d4a10be9db540991cc0cb436eaf
NeonPhoenix <neonphoenix.software@gmail.com>
2019-06-15 11:22:10 +1000

    Updated RolePlayRoles Precon
    setup precon to work with role and channel

--- DiscordBot/Preconditions/RolePlayRoles.cs
+++ DiscordBot/Preconditions/RolePlayRoles.cs
@@ -10,22 +10,21 @@ namespace DiscordBot.Preconditions
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class RolePlayRoles : PreconditionAttribute
     {
-        private static PreconditionResult _result;
-
         public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider service)
         {
             var channelID = DatabaseManager.CheckPreconditionChannel(context, "RolePlayRoles");
+            var roleID = DatabaseManager.CheckPreconditionRole(context, "RolePlayRoles");
 
-            if (!(context.User is IGuildUser user)) { return Task.FromResult(PreconditionResult.FromError("The command was not used in a guild.")); }
+            if (!(context.User is IGuildUser user)) { return Task.FromResult(PreconditionResult.FromError("The command was not used in a guild!")); }
 
-            if (!user.RoleIds.Contains(DatabaseManager.CheckPreconditionRole(context, "RolePlayRoles")))
+            if(!user.RoleIds.Contains(roleID))
             {
-                if (!(channelID == context.Channel.Id))
+                if (!(0 == context.Channel.Id || channelID == context.Channel.Id))
                 {
-                    return Task.FromResult(PreconditionResult.FromError("You are not in the proper channel to use this command!"));
+                    return Task.FromResult(PreconditionResult.FromError("You are not in the required channel for this command"));
                 }
 
-                return Task.FromResult(PreconditionResult.FromError("You do not have the proper role to use this command!"));
+                return Task.FromResult(PreconditionResult.FromError("You do not have the required role to use this command!"));
             }
 
             return Task.FromResult(PreconditionResult.FromSuccess());

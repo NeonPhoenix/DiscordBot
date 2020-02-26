e14807cd30df19a4d4abc4c5122113fbca867e5c
NeonPhoenix <neonphoenix.software@gmail.com>
2020-02-26 20:48:46 +1000

    Added new modules

---
+++ DiscordBot/Modules/WelcomerModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    class WelcomerModule
+    {
+        //TODO add welcome message
+        //TODO add leave message
+    }
+}
